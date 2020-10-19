1f84fdfb4da3defe94100f939f0c5cc63e649ad7
SreeGaneshTN <tnsreeganesh@gmail.com>
2020-10-19 23:40:50 +0530

    generator script

---
+++ generator.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator,self).__init__()
+
+        #input 100*1*1
+        self.layer1=nn.Sequential(
+            nn.ConvTranspose2d(100,512,4,1,0,bias=False)
+            nn.BatchNorm2d(512)
+            nn.ReLU(True)
+            )
+        self.layer2 = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Dropout2d(0.5)
+        )
+        # input 256*8*8
+        self.layer3 = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Dropout2d(0.5)
+        )
+        # input 128*16*16
+        self.layer4 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Dropout2d(0.5)
+        )
+        # input 64*32*32
+        self.layer5 = nn.Sequential(
+        nn.ConvTranspose2d(64, 1, 4, 2, 1, bias=False),
+        nn.Tanh()
+        )
+
+        # output 1*64*64
+
+        self.embedding = nn.Embedding(10, 100)
+
+    def forward(self,noise,labels):
+        emb=self.embedding(labels)
+        x=torch.mul(noise,emb)
+        x=self.layer1(x)
+        x=self.layer2(x)
+        x=self.layer3(x)
+        x=self.layer4(x)
+        x=self.layer5(x)
+
+        return x
